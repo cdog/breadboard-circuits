bb972a3420952825c52db596d53fba86abce3e58
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-02-25 17:56:33 +0200

    Updated grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 module.exports = function (grunt) {
   'use strict';
 
+  require('jit-grunt')(grunt, {
+    buildcontrol: 'grunt-build-control',
+    htmllint: 'grunt-html'
+  });
+
+  require('time-grunt')(grunt);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
   });
