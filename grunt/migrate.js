dfd572c101a2b41c039889e80b63512b35f981b1
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-04-06 23:19:04 +0300

    Added readdir function

--- grunt/migrate.js
+++ grunt/migrate.js
@@ -1,7 +1,34 @@
 'use strict';
 
+var fs = require('fs');
+
 module.exports = function migrate(grunt) {
   var options = {};
+
+  function readdir(path, recursive, ext) {
+    var results = [];
+    var files = fs.readdirSync(path);
+
+    files.forEach(function(file) {
+      file = path + '/' + file;
+
+      var stat = fs.statSync(file);
+
+      if (stat && stat.isDirectory()) {
+        if (recursive) {
+          results = results.concat(readdir(file, recursive, ext));
+        }
+      } else if (ext) {
+        if (file.split('.').pop() === ext) {
+          results.push(file);
+        }
+      } else {
+        results.push(file)
+      }
+    });
+
+    return results;
+  };
 
   grunt.registerMultiTask('migrate', function () {
     options = this.data;
