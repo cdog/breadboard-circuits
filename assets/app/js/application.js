c006b4b04e20231bd25222e7cebfae49a7927ffe
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-04-07 23:29:57 +0300

    Built breadboard-circuits from commit 98170aa on branch master

--- assets/app/js/application.js
+++ assets/app/js/application.js
@@ -6,9 +6,52 @@ if (typeof window.Wyliodrin === 'undefined') {
   'use strict';
 
   Wyliodrin.breadboard = {
-    parts: []
+    parts: [],
+    partsPath: 'assets/app/parts/parts.json'
   };
 })(Wyliodrin);
+
+(function ($) {
+  'use strict';
+
+  function loadParts() {
+    var parts = localStorage.getItem('parts');
+
+    if (parts !== null) {
+      Wyliodrin.breadboard.parts = JSON.parse(parts);
+
+      return;
+    }
+
+    NProgress.configure({
+      showSpinner: false
+    });
+
+    $.ajax({
+      success: function (data) {
+        localStorage.setItem('parts', JSON.stringify(data));
+
+        Wyliodrin.breadboard.parts = data;
+      },
+      url: Wyliodrin.breadboard.partsPath,
+      xhr: function () {
+        var xhr = new window.XMLHttpRequest();
+
+        xhr.addEventListener('progress', function (event) {
+          if (event.lengthComputable) {
+            var progress = event.loaded / event.total;
+
+            NProgress.set(progress);
+          }
+        }, false);
+
+        return xhr;
+      }
+    });
+  }
+
+  loadParts();
+})(jQuery);
 
 (function ($) {
   'use strict';
@@ -298,30 +341,6 @@ if (typeof window.Wyliodrin === 'undefined') {
           pushbutton.classed('pressed', false);
           lightBulb.classed('off', true);
         });
-    }
-  });
-
-  NProgress.configure({
-    showSpinner: false
-  });
-
-  $.ajax({
-    success: function (data) {
-      Wyliodrin.breadboard.parts = data;
-    },
-    url: 'assets/app/parts/parts.json',
-    xhr: function () {
-      var xhr = new window.XMLHttpRequest();
-
-      xhr.addEventListener('progress', function (event) {
-        if (event.lengthComputable) {
-          var percentComplete = event.loaded / event.total;
-
-          NProgress.set(percentComplete);
-        }
-      }, false);
-
-      return xhr;
     }
   });
 })(jQuery);
